ef7b7ffcbc7410ccfadb8f9a15c4d849f60926a6
Nick <nfbullock@gmail.com>
2018-04-12 11:46:40 -0700

    refactor

--- SSH.sh
+++ SSH.sh
@@ -1,66 +1,70 @@
 #!/bin/bash
 
+HOSTS=${HOME}/.hosts
+
 function printbar {
-	printf "%s\n" "" "-----------------------------" ""
+    printf "%s\n" "" "-----------------------------" ""
 }
 
 function display {
-	hostnum=1
-	printbar
+    hostnum=1
+    printbar
     echo "0 - Create a new entry"
-	cat $HOME/.hosts | awk '{print $1}' | while read i; do echo $hostnum - $i; hostnum=$((hostnum+1)); done;
-	echo
-	read -p "Where would you like to jump? (0): " server; server=${server:-0}
-	printbar
-	if [ $server -eq 0 ]; then
-		input
-	else
-		comm=$(sed -n ${server}p $HOME/.hosts | awk '{$1=""; print $0}')
-		$comm
-	fi
+    awk '{print $1}' ${HOSTS} | while read i; do
+        echo $hostnum - $i
+        hostnum=$((hostnum+1))
+           done;
+    read -p "Where would you like to jump? (0): " server; server=${server:-0}
+    printbar
+    if [ $server -eq 0 ]; then
+        input
+    else
+        comm=$(sed -n ${server}p $HOSTS | awk '{$1=$2=""; print $0}')
+        /usr/bin/ssh -o StrictHostKeyChecking=no $comm
+    fi
 }
 
 function container {
-	if [ ! -f $HOME/.hosts ]; then
-		touch $HOME/.hosts
-	fi
+    if [ ! -f $HOSTS ]; then
+        touch $HOSTS
+    fi
 }
 
 function input {
-	read -p "Host nickname? ONE WORD: " hostname
-	read -p "What's the address? IP or FQDN: " host
-	read -p "Port? (22): " port; port=${port:-22} 
-	read -p "User? (root): " user; user=${user:-root}
-	echo $hostname "ssh -p "$port $user"@"$host >> $HOME/.hosts
-	printbar
-	rsacopy
-	printbar
-	echo "This host has been added."
-	display
+    read -p "Host nickname? ONE WORD: " hostname
+    read -p "What's the address? IP or FQDN: " host
+    read -p "Port? (22): " port; port=${port:-22} 
+    read -p "User? (root): " user; user=${user:-root}
+    echo $hostname "ssh -p "$port $user"@"$host >> $HOSTS
+    printbar
+    rsacopy
+    printbar
+    echo "This host has been added."
+    display
 }
 
 function rsacopy {
-	read -p "Would you like to copy your public key? (Y/n): " rsacp; rsacp=${rscap:-y}
-	if [ $rsacp == "y" ] || [ $rsacp == "Y" ]; then
-		keycheck
-		scp -P $port $HOME/.ssh/id_rsa.pub $user@$host:~/
-		ssh -p $port $user@$host 'mkdir ~/.ssh' 2> /dev/null
-		ssh -p $port $user@$host 'cat ~/id_rsa.pub >> ~/.ssh/authorized_keys && rm -f ~/id_rsa.pub'
-	fi
+    read -p "Would you like to copy your public key? (Y/n): " rsacp; rsacp=${rscap:-y}
+    if [ $rsacp == "y" ] || [ $rsacp == "Y" ]; then
+        keycheck
+        scp -P $port $HOME/.ssh/id_rsa.pub $user@$host:~/
+        ssh -p $port $user@$host 'mkdir ~/.ssh' 2> /dev/null
+        ssh -p $port $user@$host 'cat ~/id_rsa.pub >> ~/.ssh/authorized_keys && rm -f ~/id_rsa.pub'
+    fi
 }
 
 function keycheck {
-	if [ !	-f $HOME/.ssh/id_rsa.pub ]; then
-		echo "It doesn't look like you've generated the default 'id_rsa.pub'."
-		echo "Let me do that for you now."
-		mkdir $HOME/.ssh 2> /dev/null
-		read -p  "Would you like to use a passphrase? (Y/n): " passphrase; passphrase=${passphrase:-y}
-			if [ $passphrase == "y" ] || [ $passphrase == "Y" ]; then
-				ssh-keygen -t rsa -b 2048 -f $HOME/.ssh/id_rsa -q
-			else
-				ssh-keygen -t rsa -b 2048 -f $HOME/.ssh/id_rsa -q -N ""
-			fi
-	fi
+    if [ !    -f $HOME/.ssh/id_rsa.pub ]; then
+        echo "It doesn't look like you've generated the default 'id_rsa.pub'."
+        echo "Let me do that for you now."
+        mkdir $HOME/.ssh 2> /dev/null
+        read -p  "Would you like to use a passphrase? (Y/n): " passphrase; passphrase=${passphrase:-y}
+            if [ $passphrase == "y" ] || [ $passphrase == "Y" ]; then
+                ssh-keygen -t rsa -b 2048 -f $HOME/.ssh/id_rsa -q
+            else
+                ssh-keygen -t rsa -b 2048 -f $HOME/.ssh/id_rsa -q -N ""
+            fi
+    fi
 }
 
 container
